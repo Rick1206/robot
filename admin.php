ffd9e0af95635e3f3ff6caa1f69b754388932f3a
rick <rickshy1206@gmail.com>
2013-08-06 01:05:11 +0800

    this is backend develop

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+//调试模式
+define('APP_DEBUG',TRUE);
+define('THINK_PATH', "./Core/");
+define('APP_NAME', "admin");
+define('APP_PATH', "./admin/");
+
+//define('HOME_PATH', './admin/');
+//define('M_PATH', 'm/');
+require THINK_PATH.'ThinkPHP.php';
